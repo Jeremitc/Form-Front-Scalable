0bddfcc3938f0ec7a0527da0d33e9e6de95f535e
JeremiTC <jeremy.genial1@gmail.com>
2025-06-10 16:57:28 -0500

    Optimization Tailwind Css

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -4,7 +4,7 @@
     <h1>{{ $t('notFound.title') }}</h1>
     <p>{{ $t('notFound.description') }}</p>
     <!-- El enlace a "/" será interceptado por el router y redirigido a "/es" o "/en" -->
-    <router-link to="/">{{ $t('notFound.back') }}</router-link>
+    <router-link to='/'>{{ $t('notFound.back') }}</router-link>
   </div>
 </template>
 
